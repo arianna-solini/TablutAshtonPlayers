2f72efb96faa1bf7c2bd339bfdbd9b20d95bca7a
Riccardo-Vasumini <riccardovasumini@gmail.com>
2019-04-14 14:41:44 +0200

    Added Game class and exceptions

---
+++ Tablut/src/exceptions/ClimbingCitadelException.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+import domain.Action;
+
+public class ClimbingCitadelException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ClimbingCitadelException(Action a)
+	{
+		super("A pawn is tryng to climb over a citadel: "+a.toString());
+	}
+
+}
