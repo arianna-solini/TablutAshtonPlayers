7b98f5a67e0ef57fdd8d06ba01e5370e40ae4195
arianna-solini <arianna.solini@gmail.com>
2019-05-05 17:29:52 +0200

    Modified calculateScore

--- Tablut/src/ai/Score.java
+++ Tablut/src/ai/Score.java
@@ -50,60 +50,72 @@ public class Score{
 
 		//TODO lastAction potrebbe essere usata per vedere se certe pedine avversarie si stanno muovendo in una certa direzione
 		int rowTo = this.state.getLastAction().getRowTo(), columnTo = this.state.getLastAction().getColumnTo();
+
+		int whiteCaptured = (9-numWhite);
+		int weightwhiteCaptured = 1;
+
+		int blackCaptured = (16-numBlack);
+		int weightBlackCaptured = 1;
+		int weightBlackNearTheKing = 1;
+
 		switch (player){
 			case "W" :
-				
-				if(turnNumber < 20)
-					captureMultiplier = 2;
-				else if(turnNumber < 30)
-					captureMultiplier = 5;
+				//if(turnNumber < 20)
+				//	captureMultiplier = 2;
+				//else if(turnNumber < 30)
+				//	captureMultiplier = 5;
 				//Differenza tra quelle che ho mangiato io e quelle che ha mangiato lui
-				scoreWhite = ((16-numBlack)*captureMultiplier - (9-numWhite));
+				
 				//Negative score of opponent's pawns
 				//scoreWhite -= numBlack;
 				switch(currentKingPosition){
 					case "e5":
 						if(numBlackNearTheKing >= 2)
-							scoreWhite -= numBlackNearTheKing;
+							weightBlackNearTheKing = -2;
 
 					case "e4":
 						if(numBlackNearTheKing >= 2)
-							scoreWhite -= numBlackNearTheKing;
+							weightBlackNearTheKing = -4;
 
 					case "e6":
 						if(numBlackNearTheKing >= 2)
-							scoreWhite -= numBlackNearTheKing;
+							weightBlackNearTheKing = -4;
 
 					case "d5":
 						if(numBlackNearTheKing >= 2)
-							scoreWhite -= numBlackNearTheKing;
+							weightBlackNearTheKing = -4;
 
 					case "f5":
 						if(numBlackNearTheKing >= 2)
-							scoreWhite -= numBlackNearTheKing;
+							weightBlackNearTheKing = -4;
 
 					default:
 						if(numBlackNearTheKing > 0)
-							scoreWhite -= numBlackNearTheKing;
+							weightBlackNearTheKing = -1;
 				}
-				scoreWhite += (oldNumBlack- numBlack)*2;
+				if(weightBlackNearTheKing > -2)
+					weightBlackCaptured = 2;
+				
 				ArrayList<Action> possibleWinActions = game.canKingWin(state);
 				if(possibleWinActions != null)
-					scoreWhite += 20;
-				return scoreWhite + r.nextDouble();
+					return 20 + blackCaptured*weightBlackCaptured + weightBlackNearTheKing;
+				else
+					return blackCaptured*weightBlackCaptured + weightBlackNearTheKing;
+				
 				
 			case "B":
-				if(turnNumber < 20)
-					captureMultiplier = 5;
-				else if(turnNumber < 30)
-					captureMultiplier = 2;
+				//if(turnNumber < 20)
+				//	captureMultiplier = 5;
+				//else if(turnNumber < 30)
+				//	captureMultiplier = 2;
 				//Differenza tra quelle che ho mangiato io e quelle che ha mangiato lui
-				scoreBlack = ((9-numWhite)*captureMultiplier - (16 - numBlack));
+				if(state.getTurnNumber() > 20)
+					weightwhiteCaptured = 2;
 				//scoreBlack -= numWhite;
-				scoreBlack += (oldNumWhite - numWhite)*2;
+				//scoreBlack += (oldNumWhite - numWhite)*2;
 				//if (board.isColumnEmpty(columnTo) || board.isRowEmpty(rowTo))
 					//scoreBlack += 1;
-				return scoreBlack + r.nextDouble();
+				return whiteCaptured*weightwhiteCaptured;
 
 			default:
 				return -1;
